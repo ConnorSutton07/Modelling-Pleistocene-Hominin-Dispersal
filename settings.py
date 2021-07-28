a6c8c06d12823d88b5acba6706e68e985a341c4d
Connor <connorsutton07@gmail.com>
2021-07-28 16:24:59 -0500

    added settings file

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""
+Contains constants that will be used to initialize and parameterize the
+simulation model
+
+"""
+
+#-----------------------
+#      World Setup
+#-----------------------
+WORLD_SHAPE = (205, 155)
+
+#-----------------------
+#   Genetic Variation
+#-----------------------
+α = 3
+
