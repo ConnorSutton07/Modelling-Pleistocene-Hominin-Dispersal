c92fe5448ac0ad23390cd4a8a38e682c2465612a
Connor <connorsutton07@gmail.com>
2021-07-28 17:49:17 -0500

    completed HexagonalWorld step method

--- core/world.py
+++ core/world.py
@@ -1,7 +1,20 @@
 from core.cell import Cell 
 import numpy as np
 
-class World:
+class HexagonalWorld:
+    """
+    Hexagonal grid spice containing a population of cells
+
+       Attributes
+    ----------------
+    shape: tuple(N, M)
+        N x M shape of cell grid
+    cells: np.array
+        The grid of cells
+    time_step: int
+        number of time steps since world creation
+
+    """
     def __init__(self, shape: tuple):
         self.shape = shape
         self.cells = np.array([[None for i in range(self.shape[0])] for j in range(self.shape[1])], dtype = Cell)
@@ -11,15 +24,13 @@ class World:
         cell_info = self.get_current_state()
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                hexagonal_info = self.get_hexagonal_neighbor_info((i, j), cell_info)
-                colonized_cell, dead = self.cells[i][j].update(hexagonal_info)
-                if colonized_cell is not None:
-                    pass
-                    #self.cells[colonized_cell] = Cell()
-                if dead:
-                    self.cells[i][j].becomeExtinct()
-
-
+                if self.cells[i][j].isOccupied():
+                    hexagonal_info = self.get_hexagonal_neighbor_info(i, j, cell_info)
+                    colonized_cell, dead = self.cells[i][j].update(hexagonal_info)
+                    if colonized_cell is not None:
+                        self.cells[colonized_cell].becomeOccupied(cell_info[i][j]['genotype'], cell_info[i][j]['mutation_vector'])
+                    if dead:
+                        self.cells[i][j].becomeExtinct()
         self.time_step += 1 
 
     def get_current_state(self) -> list:
@@ -27,11 +38,30 @@ class World:
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
                 cell_info[i][j] = self.cells[i][j].get_info()
+                cell_info[i][j]['location'].append((i, j))
         return cell_info
 
 
-    def get_hexagonal_neighbor_info(self) -> dict:
-        pass
+    def get_hexagonal_neighbor_info(self, i, j, cell_info) -> dict:
+        """ 
+        Retrieves the info of cells in a hexagonal neighborhood around the given index.
+        Scheme:
+
+          [2] [4]
+        [1] [3] [5]              [1][2][3][4][5]
+          [7] [9]        ->      [6][7][8][9][A]
+        [6] [8] [A]                 [B]   [C]
+          [B] [C]
+
+        """
+        hexagonal_info = []
+        for a in range(-1, 1):
+            for b in range(-1, 2):
+                if not (a == b == 0): 
+                    hexagonal_info.append(cell_info[i + a][j + b])
+        hexagonal_info.append(cell_info[i + 1][j])
+        return hexagonal_info
+
 
     
 
