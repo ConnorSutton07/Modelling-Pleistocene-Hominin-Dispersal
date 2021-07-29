4cf5182f866a657dc1d415754f57698e9488db31
Connor <connorsutton07@gmail.com>
2021-07-28 19:44:47 -0500

    created initial population around east african rift system

--- settings.py
+++ settings.py
@@ -8,6 +8,14 @@ simulation model
 #      World Setup
 #-----------------------
 WORLD_SHAPE = (205, 155)
+INITIAL_POPULATION = [  # Scattered populations around East African Rift System
+    (84, 91),
+    (85, 92),
+    (85, 96),
+    (86, 90),
+    (87, 94),
+    (88, 93)
+]
 
 #-----------------------
 #   Genetic Variation
